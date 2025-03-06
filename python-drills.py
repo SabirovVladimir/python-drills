287b4a33dba9f7e6fa61aaf1b290153d6a25da9d
Sabirov Vladimir <vladimirrashidovich@gmail.com>
2025-03-06 14:21:49 +0700

    new content

--- python-drills.py
+++ python-drills.py
@@ -235,3 +235,12 @@ for letter in letters:
     print(phrase, letter)
     phrase = " ".join(phrase.split())
     phrase = phrase.replace(letter, '')
+
+# Divisibility predicate
+def func(num1, num2):
+    return num1 % num2 == 0
+num1, num2 = int(input()), int(input())
+if func(num1, num2):
+    print('YES')
+else:
+    print('NO')
